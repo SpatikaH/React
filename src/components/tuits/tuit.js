19ae7921ba8f9fa1680706c5713dc1b15d4875e1
Spatika Himanth <spatikahimanth@Spatikas-MacBook-Air.local>
2022-12-04 19:25:42 -0500

    Adding a likes service

--- src/components/tuits/tuit.js
+++ src/components/tuits/tuit.js
@@ -3,7 +3,7 @@ import TuitStats from "./tuit-stats";
 import TuitImage from "./tuit-image";
 import TuitVideo from "./tuit-video";
 
-export const Tuit = ({tuit, deleteTuit}) => {
+export const Tuit = ({tuit, deleteTuit, likeTuit}) => {
   return(
     <li className="p-2 ttr-tuit list-group-item d-flex rounded-0">
       <div className="pe-2">
@@ -29,9 +29,9 @@ export const Tuit = ({tuit, deleteTuit}) => {
           tuit.image &&
           <TuitImage tuit={tuit}/>
         }
-        <TuitStats tuit={tuit}/>
+        <TuitStats tuit={tuit} likeTuit={likeTuit}/>
       </div>
     </li>
   );
 }
-//export default Tuit;
+export default Tuit;
